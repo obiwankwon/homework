fb87e3bb8e674a2d1c35ad6579d4189aaaebe774
obiwankwon <113491213+obiwankwon@users.noreply.github.com>
2022-09-28 12:17:39 +0900

    Add files via upload

--- hw2.c
+++ hw2.c
@@ -4,5 +4,9 @@
 int main(void)
 {
 	double num1;
-
+	printf("Please enter kilomiters:");
+	scanf("%lf", &num1);
+	double result1 = num1 / 1.609;
+	printf("%.1f km is equal to %.1f miles\n", num1, result1);
+	return 0;
 }
